21762d9aed21bcb18306510694773f06a2829772
Victor Shermon <victorshermon@yahoo.com>
2021-03-16 20:26:17 -0300

    chore: add husky lint staged

---
+++ jest.config.ts
@@ -0,0 +1,12 @@
+export default {
+  roots: ['<rootDir>/src'],
+  clearMocks: true,
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  coverageDirectory: 'coverage',
+  coverageProvider: 'v8',
+  testEnvironment: 'node',
+  transform: {
+    '.+\\.ts$': 'ts-jest'
+  },
+  testMatch: ['<rootDir>/src/**/*.spec.ts']
+}
---
+++ src/test.ts
@@ -0,0 +1,5 @@
+function nome (name: string): string {
+  return name
+}
+
+nome('victor')
