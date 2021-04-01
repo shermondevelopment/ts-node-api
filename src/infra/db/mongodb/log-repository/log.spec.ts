c2711c6c8c52a53d53e2db3df85d2f9a33bacb61
Victor Shermon <victorshermon@yahoo.com>
2021-04-01 19:19:34 -0300

    test: ensure LogMongoRepository creates an error log on success

---
+++ src/infra/db/mongodb/log-repository/log.spec.ts
@@ -0,0 +1,24 @@
+import { MongoHelper } from '../helpers/mongo-helper'
+import { Collection } from 'mongodb'
+import { LogMongoRepository } from './log'
+import mongConfig from '../../../../../globalConfig'
+
+describe('Log Mongo Repository', () => {
+  let errorCollection: Collection
+  beforeAll(async () => {
+    await MongoHelper.connect(mongConfig.mongoUri)
+  })
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+  beforeEach(async () => {
+    errorCollection = await MongoHelper.getCollection('errors')
+    await errorCollection.deleteMany({})
+  })
+  test('Should create an error log success', async () => {
+    const sut = new LogMongoRepository()
+    await sut.logError('any_stack')
+    const count = await errorCollection.countDocuments()
+    expect(count).toBe(1)
+  })
+})
